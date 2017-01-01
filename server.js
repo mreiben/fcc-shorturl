dc012d4535c1cd2ebbccbd65561270678c09bcd6
mreiben <jason.eiben@gmail.com>
2017-01-01 23:29:16 +0000

    updated port for heroku deployment

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, function() {
+    console.log('Listening on port %d', server_port);
+});
