2196b35ce1920b6e63e0b3ead9007bede8e5307f
mreiben <jason.eiben@gmail.com>
2017-01-01 23:31:38 +0000

    added landing text

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
