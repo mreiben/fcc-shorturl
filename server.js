b94e9f028d0cb0a5cb7717f682857ca3a026ddf0
mreiben <jason.eiben@gmail.com>
2017-01-02 03:59:59 +0000

    added mongo functionality

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/:dbVal', function(req, res){
             "ext": dbVal
         });
         //open longURL[dbVal]
-        res.redirect("http://www.nytimes.com");
+        res.render(longURL);
         db.close();
     });
 });
